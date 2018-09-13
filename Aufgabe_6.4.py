2fdc3efb8f260129a66125dd8ebec7edb577b23c
SoSch17 <Sonja--Schuster@web.de>
2018-09-13 09:55:38 +0200

    Importance hinzugefügt

--- Aufgabe_6.4.py
+++ Aufgabe_6.4.py
@@ -7,6 +7,14 @@ weatherData = [["sky", "air", "humid", "wind", "water", "forecast", "attack"],
                ["sunny", "warm", "high", "strong", "warm", "same", "+"],
                ["rainy", "cold", "high", "strong", "warm", "change", "-"]]
 
+weatherData2 = [["sky", "sunny", "sunny", "rainy", "sunny", "sunny", "rainy"],
+                ["air", "warm", "warm", "cold", "warm", "warm", "warm"],
+                ["humid", "normal", "high", "high", "high", "normal", "high"],
+                ["wind", "strong", "strong", "strong", "strong", "weak", "strong"],
+                ["water", "warm", "warm", "warm", "cool", "warm", "warm"],
+                ["forecast", "same", "same", "change", "change", "same", "change"],
+                ["attack", "+", "+", "-", "+", "-", "-"]]
+
 
 class Branch:
     def __init__(self, label):
@@ -104,3 +112,82 @@ sampledata = [["tries", "result"],
               ["alot", "no"],
               ["yes", "no"]]
 print(is_same_classifications(sampledata))
+
+
+def importance(attribute_index, examples):
+    attribute = examples[attribute_index]
+
+    last_attribute = examples[-1]
+    #last_attribute.pop(0)
+    #last_attribute.sort()
+    positive_and_negative_count = len(last_attribute)-1
+    positive_count = 0
+
+    attribute_positive_value_name = last_attribute[1]
+    for index, value in enumerate(last_attribute):
+        if index != 0:
+            if value == attribute_positive_value_name:
+                positive_count +=1
+
+    q = positive_count / positive_and_negative_count
+    result = b(q) - remainder(attribute, examples, attribute_positive_value_name)
+    return result
+
+def b(q):
+    # B(q) = -(q log2 q + (1 - q) log2(1 - q)
+    if q != 0:
+        b = ( q * (np.log(q) / np.log(2)) )
+        if q != 1:
+            b +=  ((1 - q) * (np.log(1 - q) / np.log(2)) )
+        b = -b
+    else:
+        b = 0
+    return b
+
+
+def remainder(attribute, examples, attribute_positive_value_name):
+    #attribute.pop(0)
+    e_count = len(attribute)-1
+    e1_count = 0
+
+    last_attribute = examples[-1]
+    #last_attribute.pop(0)
+    p_e1 = 0
+    p_e2 = 0
+
+    attribute_value_name = attribute[1]
+    for index, value in enumerate(attribute):
+        if index != 0:
+            if value == attribute_value_name:
+                e1_count += 1
+                if last_attribute[index] == attribute_positive_value_name:
+                    p_e1 += 1
+            else:
+                if last_attribute[index] == attribute_positive_value_name:
+                    p_e2 += 1
+
+    e2_count = e_count - e1_count
+    q1 = p_e1 / e1_count
+    q2 = p_e2 / e2_count
+
+    result = (e1_count / e_count) * b(q1) + ( e2_count / e_count ) * b(q2)
+    return result
+
+
+def testImportance(examples, attributes):
+    # suche das wichtigste Attribut
+    ig_max_attribute_name = None
+    ig_max = 0
+    ig_current = 0
+    for attribute_column_index, attribute_column in enumerate(examples):
+        if attribute_column_index != (len(examples) - 1):
+            ig_current = importance(attribute_column_index, examples)
+            print(ig_current)
+            if (ig_current > ig_max):
+                ig_max = ig_current
+                ig_max_attribute_name = attributes[attribute_column_index]
+                # Name des wichtigsten Attributes in ig_max_attribute_name speichern
+    return ig_max
+
+attributes = ["sky", "air", "humid", "wind", "water", "forecast", "attack"]
+print(testImportance(weatherData2, attributes))
