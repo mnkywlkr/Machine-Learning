82850f2a15646707a84a6c81f62173ae819afd90
SoSch17 <Sonja--Schuster@web.de>
2018-09-27 13:17:17 +0200

    comments added

--- Aufgabe_6.4.py
+++ Aufgabe_6.4.py
@@ -42,7 +42,27 @@ class Tree:
         return result
 
 
-# print(tree.branch)
+def dtLearning(examples, attributes, parent_examples):
+    if len(examples) == 0:
+        return plurality(parent_examples)
+    elif is_same_classifications(examples):
+        return get_attr_values(get_classification(examples), examples)
+    elif len(attributes) == 0:
+        return plurality(examples)
+    else:
+        # find the attribute with the highest information gain
+        important_attribute = getImportanceAttributeName(examples, attributes)
+        # print("important attribute:", important_attribute)
+        tree = Tree(important_attribute)
+        attr_value_list = get_attr_values(important_attribute, examples)
+        for each_attribute_value in attr_value_list:
+            # only datasets of example, where important_attribute has the value of each_attribute_value
+            exs = ExampleSetElimination(examples, each_attribute_value, important_attribute)
+            subtree = dtLearning(exs, reduce_attr_list(important_attribute, attributes), examples)
+            tree.add_a_branch(subtree, each_attribute_value)
+        return tree
+
+# returns a copy of the given 2d-array
 def copy_2d_array(original_array):
     copy_of_array = []
     for e in original_array:
@@ -52,30 +72,7 @@ def copy_2d_array(original_array):
         copy_of_array.append(column)
     return copy_of_array
 
-
-def dtLearning(examples, attributes, parent_examples):
-    if len(examples) == 0:
-        return plurality(parent_examples)
-    elif is_same_classifications(examples):
-        return get_attr_values(get_classification(examples), examples)
-    elif len(attributes) == 0:
-        return plurality(examples)
-    else:
-        # important_attribute = das wichtigste Attribut
-        important_attribute = getImportanceAttributeName(examples, attributes)
-        print("important attribute:", important_attribute)
-        tree = Tree(important_attribute)
-        attr_value_list = get_attr_values(important_attribute, examples)
-        for each_attribute_value in attr_value_list:
-            # Datensätze aus examples bei denen A dem Wert des Elements entspricht
-            exs = ExampleSetElimination(examples, each_attribute_value, important_attribute)
-            subtree = dtLearning(exs, reduce_attr_list(important_attribute, attributes), examples)
-            # tree = appendBranch(label, subtree)
-            tree.add_a_branch(subtree, each_attribute_value)
-           # print("tree's children:", tree.print_branches())
-        return tree
-
-
+# returns a copy of attr_list without attr
 def reduce_attr_list(attr, attr_list):
     result = []
     for a in attr_list:
@@ -83,43 +80,40 @@ def reduce_attr_list(attr, attr_list):
     result.remove(attr)
     return result
 
-
+#returns the column of the given attribute
 def get_attr_values_complete_list(attribute, data):
     copy_of_examples = copy_2d_array(data)
     attr_values = []
-    for i in copy_of_examples:
-        if attribute == i[0]:
-            for a in i:
+    for column in copy_of_examples:
+        if attribute == column[0]:
+            for a in column:
                 attr_values.append(a)
     return attr_values
 
-
+# returns only the datasets of examples, where attr has the value of attr_value
 def ExampleSetElimination(examples, attr_value, attr):
     newExamples = copy_2d_array(examples)
-    attr_value_list = get_attr_values_complete_list(attr, examples)
-    #print("reduced set:",attr_value_list)
-    positionList = find_positions(attr_value, attr_value_list)
+    attr_value_list = get_attr_values_complete_list(attr, examples) # column of the given attribute
+    positionList = find_positions(attr_value, attr_value_list) # list of indices of datasets to remove
+    # remove datasets
     for pos in positionList:
-        for i, e in enumerate(newExamples):
-            e.pop(pos)
-    # print(newExamples)
+        for column in newExamples:
+            column.pop(pos)
+    #print(newExamples)
     for ne in newExamples:
         if attr == ne[0]:
             newExamples.remove(ne)
     return newExamples
 
-
-def find_positions(attr_value, examples):
+# returns a list which contains all indices of values in column that are not the attr_value (in descending order)
+def find_positions(attr_value, column):
     positions = []
-    for i, e in enumerate(examples):
-        if e != attr_value and i != 0:
-            positions.append(i)
-    # print(positions)
-    # return positions.sort(reverse=True)
-    # start = len(positions)
+    for index, value in enumerate(column):
+        if value != attr_value and index != 0: # except label
+            positions.append(index)
     return flipList(positions)
 
-
+# returns the list in reversed order
 def flipList(list):
     result = []
     max = len(list) - 1
@@ -127,40 +121,33 @@ def flipList(list):
         result.append(list[max - n])
     return result
 
-
-def updateListAfterPop(list):
-    for l in list:
-        l -= 1
-
-
-# test for elimination
+# Test for ExampleSetElimination
 # print("before: " + "\n")
-# print(str(weatherData2) + "\n")
-#ExampleSetElimination(weatherData2, "sunny")
-
-
+# print(str(weatherData) + "\n")
+# exs_test = ExampleSetElimination(weatherData, "sunny", "sky")
 # print("after: " + "\n")
-# print(str(weatherData2) + "\n")
+# print(str(exs_test) + "\n")
 
+# returns a list which contains all possible values for the given attribute once
 def get_attr_values(attribute, data):
     copy_of_examples = copy_2d_array(data)
     attr_values = []
-    for i in copy_of_examples:
-        if attribute == i[0]:
-            for a in i:
+    for column in copy_of_examples:
+        if attribute == column[0]:
+            for a in column:
                 if a not in attr_values:
                     attr_values.append(a)
     if len(attr_values) != 0:
-        attr_values.pop(0)
+        attr_values.pop(0) #remove label
     return attr_values
 
+# Test for get_attr_values
+# print(get_attr_values("sky", weatherData))
 
-# test for get_attr_values
-# print(get_attr_values("sky", weatherData2))
-
-
+# returns the most common output value
 def plurality(examples):
-    output_values = examples[-1]
+    copy_of_examples = copy_2d_array(examples)
+    output_values = copy_of_examples[-1] #output values
     output_values.pop(0) #remove label
     output_values.sort()
     attribute_value_name = output_values[0]
@@ -181,53 +168,45 @@ def plurality(examples):
         value_with_max_count = attribute_value_name
     return value_with_max_count
 
-#Test for plurality
-#data = [['animals', 'duck', 'duck', 'dog', 'dog', 'dog'], ['animals', 'duck', 'duck', 'dog', 'dog', 'dog']]
-#print(plurality(data))
+# Test for plurality
+# data = [['animals', 'duck', 'duck', 'dog', 'dog', 'dog'], ['animals', 'duck', 'duck', 'dog', 'dog', 'dog']]
+# print(plurality(data))
 
+# returns the label of the output values
 def get_classification(data):
     return data[-1][0]
 
-
+# returns true iff all examples have same classification
 def is_same_classifications(data):
     return len(get_attr_values(get_classification(data), data)) <= 1
 
-
-sampledata = [["tries", "result"],
-              ["yes", "no"],
-              ["alot", "no"],
-              ["yes", "no"]]
-
-
-# print(is_same_classifications(weatherData2))
-
-
+# returns the information gain of an attribute, given by the index of its column in the set of examples
 def importance(attribute_index, examples):
-    attribute = examples[attribute_index]
+    attribute = examples[attribute_index] #column of the given attribute
+    last_attribute = examples[-1] #column of the output vlaues
+    positive_and_negative_count = len(last_attribute) - 1 #except label
 
-    last_attribute = examples[-1]
-    # last_attribute.pop(0)
-    # last_attribute.sort()
-    positive_and_negative_count = len(last_attribute) - 1
+    # count the positive output value
     positive_count = 0
-
-    attribute_positive_value_name = last_attribute[1]
+    attribute_positive_value_name = last_attribute[1] #first output value
     for index, value in enumerate(last_attribute):
-        if index != 0:
+        if index != 0: #except label
             if value == attribute_positive_value_name:
                 positive_count += 1
 
+    # probability of the positive output value
     q = positive_count / positive_and_negative_count
+
     remainder_result = remainder(attribute, examples, attribute_positive_value_name)
-    if remainder_result == None:
+
+    if remainder_result is None:
         result = b(q)
     else:
         result = b(q) - remainder_result
     return result
 
-
+# B(q) = -(q log2(q) + (1 - q) log2(1 - q)
 def b(q):
-    # B(q) = -(q log2 q + (1 - q) log2(1 - q)
     if q != 0:
         b = (q * (np.log(q) / np.log(2)))
         if q != 1:
@@ -237,29 +216,27 @@ def b(q):
         b = 0
     return b
 
-
+#calculates remainder function
 def remainder(attribute, examples, attribute_positive_value_name):
-    # attribute.pop(0)
-    e_count = len(attribute) - 1
+    last_attribute = examples[-1]  # column of output values
+    e_count = len(attribute) - 1 #except label
     e1_count = 0
-
-    last_attribute = examples[-1]
-    # last_attribute.pop(0)
     p_e1 = 0
     p_e2 = 0
 
-    attribute_value_name = attribute[1]
+    attribute_value_name = attribute[1] #first value
     for index, value in enumerate(attribute):
-        if index != 0:
+        if index != 0: #except label
             if value == attribute_value_name:
-                e1_count += 1
+                e1_count += 1 #counts the first value
                 if last_attribute[index] == attribute_positive_value_name:
-                    p_e1 += 1
+                    p_e1 += 1 #counts the positive output value for the first value of the given attribute
             else:
                 if last_attribute[index] == attribute_positive_value_name:
-                    p_e2 += 1
+                    p_e2 += 1 #counts the positive output value for the second value of the given attribute
 
-    e2_count = e_count - e1_count
+    e2_count = e_count - e1_count #count of the second value
+
     if e1_count > 0:
         q1 = p_e1 / e1_count
     elif e1_count == 0:
@@ -273,24 +250,18 @@ def remainder(attribute, examples, attribute_positive_value_name):
     result = (e1_count / e_count) * b(q1) + (e2_count / e_count) * b(q2)
     return result
 
-
+# returns the label of the attribute with the highest information gain
 def getImportanceAttributeName(examples, attributes):
-    # suche das wichtigste Attribut
     ig_max_attribute_name = None
     ig_max = 0
-    ig_current = 0
     for attribute_column_index, attribute_column in enumerate(examples):
-        if attribute_column_index != (len(examples) - 1):
+        if attribute_column_index != (len(examples) - 1): #except the output value
             ig_current = importance(attribute_column_index, examples)
-            #print(ig_current)
             if (ig_current > ig_max):
                 ig_max = ig_current
+                # store the label of the attribute with the current maximum information gain
                 ig_max_attribute_name = attributes[attribute_column_index]
-                # Name des wichtigsten Attributes in ig_max_attribute_name speichern
     return ig_max_attribute_name
-
-    # print(testImportance(weatherData, attributes))
-
 
 # TEST
 end_result_tree = dtLearning(weatherData, attributes, [])
